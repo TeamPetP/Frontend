bb010692ab517665e9e97b2aa75266cce6a5d1c9
UniTaeMin <onnu0802@naver.com>
2022-05-17 10:56:09 +0900

    Update Route

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,22 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import MainPage from "./pages/Main/MainPage";
-//import MainRoutes from "./pages/Main/routes";
+import MainRoutes from "./pages/Main/routes";
 import MyPageRoutes from "./pages/MyPage/routes";
 import "./App.css";
 
 function App() {
-  return (
-    <>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/*" element={<MainPage />} />
-          <Route path="/mypage" element={<MyPageRoutes />} />
-        </Routes>
-      </Router>
-    </>
-  );
+	return (
+		<>
+			<Router>
+				<Navbar />
+				<Routes>
+					<Route path="/*" element={<MainRoutes />} />
+					<Route path="/mypage" element={<MyPageRoutes />} />
+				</Routes>
+			</Router>
+		</>
+	);
 }
 
 export default App;
--- src/pages/Main/routes.tsx
+++ src/pages/Main/routes.tsx
@@ -1,12 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import IndexPage from "./index";
+import MainPage from "./MainPage";
 
 function MainRoutes() {
-  return (
-    <Routes>
-      <Route path="/" element={<IndexPage />} />
-    </Routes>
-  );
+	return (
+		<Routes>
+			<Route path="/" element={<MainPage />} />
+		</Routes>
+	);
 }
 
 export default MainRoutes;

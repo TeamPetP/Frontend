d4c4cc8d0b8750e10d9666a54905b77cd5a71878
UniTaeMin <onnu0802@naver.com>
2022-06-22 00:08:19 +0900

    Add CreatePetpGramModal, EditProfile Modal

---
+++ src/modals/EditProfile.jsx
@@ -0,0 +1,169 @@
+import React, { useContext, useState } from "react";
+import styled from "styled-components";
+import LogoImg from "../logo.png";
+import { signInGoogle, auth } from "../services/firebaseAuth";
+import PetSignInImage from "../assets/images/signin_pet_image.svg";
+import Modal from "../components/common/Modal";
+import { UserContext } from "../contexts/UserContext";
+import ProfileDefaultImage from "../assets/images/profile_default_image.png";
+import { SignUp } from "../services/authApi";
+
+const ModalWrapper = styled.div`
+	width: 100%;
+	height: 100%;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-direction: column;
+
+	position: absolute;
+	top: 0px;
+	bottom: 0px;
+	left: 0px;
+	right: 0px;
+`;
+const Button = styled.div`
+	max-width: 500px;
+	width: 90%;
+	height: 60px;
+
+	background-color: #f3593a;
+
+	border-radius: 10px;
+
+	display: flex;
+	justify-content: center;
+	align-items: center;
+
+	font-size: 26px;
+	color: white;
+	font-family: "yg-jalnan";
+
+	cursor: pointer;
+
+	margin: 1.6em 0 0.4em 0;
+`;
+const Logo = styled.img`
+	width: 230px;
+
+	margin-bottom: 40px;
+`;
+const PetImage = styled.img``;
+
+const ImageInput = styled.input`
+	display: none;
+`;
+const ProfileImage = styled.img`
+	width: 138px;
+	height: 138px;
+
+	border-radius: 100%;
+
+	margin: 0 auto;
+
+	cursor: pointer;
+`;
+
+const InputWrapper = styled.div`
+	width: 500px;
+	margin: 0 auto;
+	@media screen and (max-width: 768px) {
+		width: 90%;
+	}
+`;
+
+const InputTitle = styled.div`
+	margin-left: 4px;
+	margin-bottom: 4px;
+
+	font-size: 18px;
+`;
+const Input = styled.input`
+	width: 100%;
+	height: 60px;
+	border-radius: 10px;
+	border: 1.2px solid #f3593a;
+
+	padding: 10px 10px;
+
+	font-size: 16px;
+
+	margin-bottom: 20px;
+`;
+
+const TextArea = styled.textarea`
+	width: 100%;
+	height: 100px;
+	border-radius: 10px;
+	border: 1.2px solid #f3593a;
+	padding: 10px 10px;
+	font-size: 16px;
+`;
+
+function EditProfileModal(props) {
+	const { user } = useContext(UserContext);
+	const uploadPhoto = React.useRef("");
+	const [photo, setPhoto] = useState(false);
+	const [nickname, setNickname] = useState("");
+	const [introduce, setIntroduce] = useState("");
+	function PhotoUpdate() {
+		if (uploadPhoto.current.files.length !== 0) {
+			setPhoto(uploadPhoto.current.files[0]);
+			const reader = new FileReader();
+			// 이미지가 로드가 된 경우
+			reader.onload = (e) => {
+				setPhoto(e.target.result);
+			};
+			// reader가 이미지 읽도록 하기
+			reader.readAsDataURL(uploadPhoto.current.files[0]);
+		} else {
+			setPhoto("");
+		}
+	}
+
+	return (
+		<Modal
+			visible={props.visibility}
+			closeVisible={() => props.EditProfileModalState()}
+			width="640"
+		>
+			<ModalWrapper>
+				<Logo src={LogoImg} alt="펫피" />
+
+				<label>
+					<ImageInput
+						type="file"
+						onChange={() => {
+							PhotoUpdate();
+						}}
+						ref={uploadPhoto}
+					/>
+					{photo != "" ? (
+						<ProfileImage src={photo} alt="profile_img" />
+					) : (
+						<ProfileImage
+							src={ProfileDefaultImage}
+							alt="profile_default_img"
+						/>
+					)}
+				</label>
+				<InputWrapper>
+					<InputTitle>이름</InputTitle>
+					<Input placeholder="이름을 입력해주세요."></Input>
+				</InputWrapper>
+				<InputWrapper>
+					<InputTitle>내 소개</InputTitle>
+					<TextArea placeholder="나를 소개해주세요."></TextArea>
+				</InputWrapper>
+				<Button onClick={() => SignUp(user, nickname, introduce)}>
+					수정하기
+				</Button>
+				<Button onClick={() => SignUp(user, nickname, introduce)}>
+					탈퇴하기
+				</Button>
+			</ModalWrapper>
+		</Modal>
+	);
+}
+
+export default EditProfileModal;

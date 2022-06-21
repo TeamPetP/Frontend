aea49f85ff6daf1ee4a409bed7bb4a02e23ea947
김선희 <tjsgml951012@gmail.com>
2022-06-21 15:49:12 +0900

    style : 펫미팅 리스트 반응형

--- src/components/common/TextInput.tsx
+++ src/components/common/TextInput.tsx
@@ -1,27 +1,46 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import * as theme from "../../styles/theme";
 
 interface IInputType {
-	width?: string;
-	height?: string;
-	placeholder?: string;
-	maxLength?: number;
-	marginTop?: string;
-	marginBottom?: string;
-	marginRight?: string;
-	marginLeft?: string;
+  width?: string;
+  height?: string;
+  placeholder?: string;
+  maxLength?: number;
+  marginTop?: string;
+  marginBottom?: string;
+  marginRight?: string;
+  marginLeft?: string;
 }
-function TextInput({ width = '100%', placeholder, maxLength, marginTop='8px', marginBottom='8px', marginRight='0', marginLeft='0' }: IInputType) {
-	const [text, setText] = useState('');
+function TextInput({
+  width = "100%",
+  placeholder,
+  maxLength,
+  marginTop = "8px",
+  marginBottom = "8px",
+  marginRight = "0",
+  marginLeft = "0",
+}: IInputType) {
+  const [text, setText] = useState("");
 
-	const inputChangeHandler = (e: any) => {
+  const inputChangeHandler = (e: any) => {
     console.log(e.target.value);
     setText(e.target.value);
-  }; 
+  };
   return (
     <>
-			<Input type="text" width={width} placeholder={placeholder} value={text} marginTop={marginTop} marginBottom={marginBottom} marginRight={marginRight} marginLeft={marginLeft} onChange={inputChangeHandler} maxLength={maxLength}/>
+      <Input
+        type="text"
+        width={width}
+        placeholder={placeholder}
+        value={text}
+        marginTop={marginTop}
+        marginBottom={marginBottom}
+        marginRight={marginRight}
+        marginLeft={marginLeft}
+        onChange={inputChangeHandler}
+        maxLength={maxLength}
+      />
       {maxLength && (
         <TextLength>
           <span>{text.length}</span>/{maxLength}
@@ -31,56 +50,86 @@ function TextInput({ width = '100%', placeholder, maxLength, marginTop='8px', ma
   );
 }
 
-export function TextArea({ height = '150px', placeholder, marginTop='8px', marginBottom='8px', marginRight='0', marginLeft='0' }: IInputType) {
-	const [text, setText] = useState('');
+export function TextArea({
+  height = "150px",
+  placeholder,
+  marginTop = "8px",
+  marginBottom = "8px",
+  marginRight = "0",
+  marginLeft = "0",
+}: IInputType) {
+  const [text, setText] = useState("");
 
-	const inputChangeHandler = (e: any) => {
+  const inputChangeHandler = (e: any) => {
     console.log(e.target.value);
     setText(e.target.value);
-  }; 
-  return <InputArea height={height} placeholder={placeholder} onChange={inputChangeHandler} marginTop={marginTop} marginBottom={marginBottom} marginRight={marginRight} marginLeft={marginLeft}>{text}</InputArea>
+  };
+  return (
+    <InputArea
+      height={height}
+      placeholder={placeholder}
+      onChange={inputChangeHandler}
+      marginTop={marginTop}
+      marginBottom={marginBottom}
+      marginRight={marginRight}
+      marginLeft={marginLeft}
+    >
+      {text}
+    </InputArea>
+  );
 }
 
-
 const Input = styled.input<IInputType>`
-  width: ${props =>props.width};
+  width: ${(props) => props.width};
   border: 1px solid #cfcfcf;
   box-sizing: border-box;
   border-radius: 5px;
   background-color: #fff;
   padding: 12px 10px;
-	margin-top : ${props =>props.marginTop};
-	margin-bottom : ${props =>props.marginBottom};
-	margin-left : ${props =>props.marginLeft};
-	margin-right : ${props =>props.marginRight};
+  margin-top: ${(props) => props.marginTop};
+  margin-bottom: ${(props) => props.marginBottom};
+  margin-left: ${(props) => props.marginLeft};
+  margin-right: ${(props) => props.marginRight};
 
   &::placeholder {
     color: #d0d0d0;
     font-size: 16px;
-	  position: relative;
-		top: 2px;
+    position: relative;
+    top: 2px;
+  }
+
+  @media screen and (max-width: 600px) {
+    &::placeholder {
+      font-size: 14px;
+    }
   }
 `;
 
 const InputArea = styled.textarea<IInputType>`
-width: 100%;
-height: ${props =>props.height};
-border: 1px solid #cfcfcf;
-box-sizing: border-box;
-border-radius: 5px;
-background-color: #fff;
-padding: 12px 10px;
-margin-top : ${props =>props.marginTop};
-margin-bottom : ${props =>props.marginBottom};
-margin-left : ${props =>props.marginLeft};
-margin-right : ${props =>props.marginRight};
+  width: 100%;
+  height: ${(props) => props.height};
+  border: 1px solid #cfcfcf;
+  box-sizing: border-box;
+  border-radius: 5px;
+  background-color: #fff;
+  padding: 12px 10px;
+  margin-top: ${(props) => props.marginTop};
+  margin-bottom: ${(props) => props.marginBottom};
+  margin-left: ${(props) => props.marginLeft};
+  margin-right: ${(props) => props.marginRight};
 
-&::placeholder {
-	color: #d0d0d0;
-	font-size: 16px;
-	position: relative;
-	top: 2px;
-}
+  &::placeholder {
+    color: #d0d0d0;
+    font-size: 16px;
+    position: relative;
+    top: 2px;
+  }
+
+  @media screen and (max-width: 600px) {
+    &::placeholder {
+      font-size: 14px;
+    }
+  }
 `;
 
 const TextLength = styled.div`

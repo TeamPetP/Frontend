ad536041abcfac07477b6e954bb56cc921e2d296
UniTaeMin <onnu0802@naver.com>
2022-07-24 23:09:20 +0900

    Update MyPage

--- src/pages/MyPage/MyMeet.tsx
+++ src/pages/MyPage/MyMeet.tsx
@@ -10,155 +10,163 @@ import MyMeetList from "./myMeet/MyMeetList";
 import nullIcon from "../../assets/images/null.png";
 
 const MyMeetPage = observer(() => {
-  const { user } = useContext(UserContext);
-  const [meetData, setMeetData] = useState<any>([]);
-  const [waitData, setWaitData] = useState<any>([]);
-  const [selectedTabs, setSelectedTabs] = useState("participating");
+	const { user } = useContext(UserContext);
+	const [meetData, setMeetData] = useState<any>([]);
+	const [waitData, setWaitData] = useState<any>([]);
+	const [selectedTabs, setSelectedTabs] = useState("participating");
 
-  useEffect(() => {
-    async function fetchMeetData() {
-      const d: any = await MyMeeting(user, 0, 20);
-      console.log("내 참여중 모임 정보", d);
-      setMeetData(d.data.content);
-    }
-    async function fetchWaitData() {
-      const d: any = await MyMeetingWait(user, 0, 20);
-      console.log("내 신청중 모임 정보", d);
-      setWaitData(d.data.content);
-    }
-    fetchMeetData();
-    fetchWaitData();
-  }, [user]);
+	useEffect(() => {
+		async function fetchMeetData() {
+			const d: any = await MyMeeting(user, 0, 20);
+			console.log("내 참여중 모임 정보", d);
+			setMeetData(d.data.content);
+		}
+		async function fetchWaitData() {
+			const d: any = await MyMeetingWait(user, 0, 20);
+			console.log("내 신청중 모임 정보", d);
+			setWaitData(d.data.content);
+		}
+		fetchMeetData();
+		fetchWaitData();
+	}, [user]);
 
-  // 클릭한 탭 구별
-  function setClickedTabs(e: any) {
-    const role = e.target.dataset.role;
-    setSelectedTabs(role);
-  }
+	// 클릭한 탭 구별
+	function setClickedTabs(e: any) {
+		const role = e.target.dataset.role;
+		setSelectedTabs(role);
+	}
 
-  return (
-    <Wrapper>
-      <PageTitle title="내모임" />
-      <TabsWrap>
-        <Tab
-          onClick={setClickedTabs}
-          data-role="participating"
-          page="participating"
-          selectedTabs={selectedTabs}
-        >
-          참여중
-        </Tab>
-        <Tab
-          onClick={setClickedTabs}
-          data-role="Applying"
-          page="Applying"
-          selectedTabs={selectedTabs}
-        >
-          신청중
-        </Tab>
-      </TabsWrap>
+	return (
+		<Wrapper>
+			<PageTitle title="내모임" />
+			<TabsWrap>
+				<Tab
+					onClick={setClickedTabs}
+					data-role="participating"
+					page="participating"
+					selectedTabs={selectedTabs}
+				>
+					참여중
+				</Tab>
+				<Tab
+					onClick={setClickedTabs}
+					data-role="Applying"
+					page="Applying"
+					selectedTabs={selectedTabs}
+				>
+					신청중
+				</Tab>
+			</TabsWrap>
 
-      {selectedTabs === "participating" && (
-        <ContentArea>
-          {meetData == null || meetData.length === 0 ? (
-            <NullWrapper>
-              <img src={nullIcon} />
-              <div>참여중인 모임이 없습니다.</div>
-            </NullWrapper>
-          ) : (
-            <>
-              {meetData.map((data: any) => (
-                <MyMeetList data={data} key={data.gatheringId} />
-              ))}
-            </>
-          )}
-        </ContentArea>
-      )}
-      {/*
-      {selectedTabs === "Applying" && (
-        <ContentArea>
-          {waitData != null &&
-            waitData.map((data: any) => {
-              return <MyMeetList data={data} key={data.gatheringId} />;
-            })}
-          {waitData == null || waitData.length === 0 ? (
-            <NullWrapper>
-              <img src={nullIcon} />
-              <div>신청중인 모임이 없습니다.</div>
-            </NullWrapper>
-          ) : (
-            <></>
-          )}
-        </ContentArea>
-      )} */}
-    </Wrapper>
-  );
+			{selectedTabs === "participating" && (
+				<ContentArea>
+					{meetData == null || meetData.length === 0 ? (
+						<NullWrapper>
+							<img src={nullIcon} />
+							<div>참여중인 모임이 없습니다.</div>
+						</NullWrapper>
+					) : (
+						<>
+							{meetData.map((data: any) => (
+								<MyMeetList
+									data={data}
+									key={data.gatheringId}
+								/>
+							))}
+						</>
+					)}
+				</ContentArea>
+			)}
+
+			{selectedTabs === "Applying" && (
+				<ContentArea>
+					{waitData != null &&
+						waitData.map((data: any) => {
+							return (
+								<MyMeetList
+									data={data}
+									key={data.gatheringId}
+								/>
+							);
+						})}
+					{waitData == null || waitData.length === 0 ? (
+						<NullWrapper>
+							<img src={nullIcon} />
+							<div>신청중인 모임이 없습니다.</div>
+						</NullWrapper>
+					) : (
+						<></>
+					)}
+				</ContentArea>
+			)}
+		</Wrapper>
+	);
 });
 
 export default withMain(MyMeetPage, "마이페이지");
 
 const Wrapper = styled.div`
-  width: 100%;
-  height: 100%;
+	width: 100%;
+	height: 100%;
 `;
 
 const ContentArea = styled.div`
-  padding: 10px 40px;
+	padding: 10px 40px;
 
-  @media screen and (max-width: 600px) {
-    padding: 10px;
-  }
+	@media screen and (max-width: 600px) {
+		padding: 10px;
+	}
 `;
 
 const TabsWrap = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
 `;
 
 const Tab = styled.div`
-  width: 50%;
-  padding: 20px;
-  font-size: 20px;
-  text-align: center;
-  line-height: 24px;
-  cursor: pointer;
-  transition: all 200ms ease;
-  border-bottom: ${(props: { selectedTabs: string; page: string }) =>
-    props.selectedTabs === props.page
-      ? `2px solid ${theme.PrimaryColor}`
-      : "2px solid #EBEBEB"};
-  color: ${(props: { selectedTabs: string; page: string }) =>
-    props.selectedTabs === props.page ? `${theme.PrimaryColor}` : "#000"};
-  font-weight: ${(props: { selectedTabs: string; page: string }) =>
-    props.selectedTabs === props.page ? 600 : 500};
+	width: 50%;
+	padding: 20px;
+	font-size: 20px;
+	text-align: center;
+	line-height: 24px;
+	cursor: pointer;
+	transition: all 200ms ease;
+	border-bottom: ${(props: { selectedTabs: string; page: string }) =>
+		props.selectedTabs === props.page
+			? `2px solid ${theme.PrimaryColor}`
+			: "2px solid #EBEBEB"};
+	color: ${(props: { selectedTabs: string; page: string }) =>
+		props.selectedTabs === props.page ? `${theme.PrimaryColor}` : "#000"};
+	font-weight: ${(props: { selectedTabs: string; page: string }) =>
+		props.selectedTabs === props.page ? 600 : 500};
 
-  &:hover {
-    color: ${theme.PrimaryColor};
-  }
+	&:hover {
+		color: ${theme.PrimaryColor};
+	}
 
-  @media screen and (max-width: 600px) {
-    padding: 14px;
-    font-size: 16px;
-  }
+	@media screen and (max-width: 600px) {
+		padding: 14px;
+		font-size: 16px;
+	}
 `;
 
 const NullWrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-direction: column;
 
-  height: calc(100% - 180px);
-  margin: 20px 0px;
-  & > div {
-    margin-top: 20px;
-    margin-bottom: 40px;
-    font-family: "yg-jalnan";
+	height: calc(100% - 180px);
+	margin: 20px 0px;
+	& > div {
+		margin-top: 20px;
+		margin-bottom: 40px;
+		font-family: "yg-jalnan";
 
-    font-size: 28px;
-  }
-  & > img {
-    width: 100%;
-  }
+		font-size: 28px;
+	}
+	& > img {
+		width: 100%;
+	}
 `;
--- src/pages/MyPage/index/Feed.tsx
+++ src/pages/MyPage/index/Feed.tsx
@@ -5,61 +5,90 @@ import SampleImg from "../../../assets/images/bg1.png";
 import nullIcon from "../../../assets/images/null.png";
 
 const Feed = observer(({ data }: any) => {
-  const { userStore } = useStores();
-  return (
-    <Wrapper>
-      {data.content != null &&
-        data.content.map((data: any) => {
-          return <Img key={data.id} src={SampleImg} alt="활동사진" />;
-        })}
-      {data.content == null || data.content.length === 0 ? (
-        <NullWrapper>
-          <img src={nullIcon} />
-          <div>게시물이 존재하지 않습니다.</div>
-        </NullWrapper>
-      ) : (
-        <></>
-      )}
-    </Wrapper>
-  );
+	const { userStore } = useStores();
+	return (
+		<Wrapper>
+			{data.content != null &&
+				data.content.map((data: any) => {
+					return data.imgUrlList.length > 0 ? (
+						<ImgWrapper>
+							<Img
+								key={data.id}
+								src={data.imgUrlList[0]}
+								alt="활동사진"
+							/>
+						</ImgWrapper>
+					) : (
+						<ImgWrapper>
+							<Img key={data.id} src={SampleImg} alt="활동사진" />
+						</ImgWrapper>
+					);
+				})}
+			{data.content == null || data.content.length === 0 ? (
+				<NullWrapper>
+					<img src={nullIcon} />
+					<div>게시물이 존재하지 않습니다.</div>
+				</NullWrapper>
+			) : (
+				<></>
+			)}
+		</Wrapper>
+	);
 });
 
 export default Feed;
 
 const Wrapper = styled.div`
-  width: 100%;
-  height: 100%;
-  padding: 0 20px;
+	width: 100%;
+	height: 100%;
+	padding: 0 20px;
+
+	display: flex;
+	flex-wrap: wrap;
 `;
 
-const Img = styled.img`
-  width: calc(33.3% - 7px);
-  border: 1px solid black;
-  box-sizing: border-box;
-  margin: 10px 10px 0 0;
-  cursor: pointer;
+const ImgWrapper = styled.div`
+	position: relative;
+	width: calc(33% - 7px);
 
-  &:nth-child(3n) {
-    margin-right: 0;
-  }
+	border: 1px solid black;
+	box-sizing: border-box;
+	margin: 10px 10px 0 0;
+	cursor: pointer;
+
+	&:nth-child(3n) {
+		margin-right: 0;
+	}
+	&:before {
+		content: "";
+		display: block;
+		padding-top: 100%;
+	}
+`;
+const Img = styled.img`
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
 `;
 
 const NullWrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-direction: column;
 
-  height: calc(100% - 180px);
-  margin: 20px 0px;
-  & > div {
-    margin-top: 20px;
-    margin-bottom: 40px;
-    font-family: "yg-jalnan";
+	height: calc(100% - 180px);
+	margin: 20px 0px;
+	& > div {
+		margin-top: 20px;
+		margin-bottom: 40px;
+		font-family: "yg-jalnan";
 
-    font-size: 28px;
-  }
-  & > img {
-    width: 100%;
-  }
+		font-size: 28px;
+	}
+	& > img {
+		width: 100%;
+	}
 `;

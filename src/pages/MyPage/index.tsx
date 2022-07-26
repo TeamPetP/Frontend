a7754578713e47082c64fb09c1ef3939c4ff9331
UniTaeMin <onnu0802@naver.com>
2022-07-26 21:17:53 +0900

    Fix MyPage Index

--- src/pages/MyPage/index.tsx
+++ src/pages/MyPage/index.tsx
@@ -14,41 +14,21 @@ import Bookmark from "./index/Bookmark";
 import { signOut } from "../../services/firebaseAuth";
 
 const IndexPage = observer(() => {
-  const { modalStore, userStore } = useStores();
-  const { user } = useContext(UserContext);
-  const navigate = useNavigate();
-  const [loading, setLoading] = useState<Boolean>(false);
-  const [info, setInfo] = useState<any>([]);
-  const [feedData, setFeedData] = useState<any>([]);
-  const [likePost, setLikePost] = useState<any>([]);
+	const { modalStore, userStore } = useStores();
+	const { user } = useContext(UserContext);
+	const navigate = useNavigate();
+	const [loading, setLoading] = useState<Boolean>(false);
+	const [info, setInfo] = useState<any>([]);
+	const [feedData, setFeedData] = useState<any>([]);
+	const [likePost, setLikePost] = useState<any>([]);
 
-  useEffect(() => {
-    async function fetchInfoData() {
-      const info: any = await InfoData(user);
-      console.log("마이페이지의 info", info);
-      setInfo(info.data);
-    }
-    fetchInfoData();
-  }, [user]);
-
-  const MoveAlrimPage = () => {
-    navigate("/mypage/alrim");
-  };
-  const MoveMyMeetPage = () => {
-    navigate("/mypage/myMeet");
-  };
-  const MoveAttentionMeetPage = () => {
-    navigate("/mypage/attentionMeet");
-  };
-
-		async function fetchBookmarkData() {
-			const likepost: any = await MyLikePost(user);
-			console.log("likepost", likepost);
-			setLikePost(likepost.data);
+	useEffect(() => {
+		async function fetchInfoData() {
+			const info: any = await InfoData(user);
+			console.log("마이페이지의 info", info);
+			setInfo(info.data);
 		}
 		fetchInfoData();
-		fetcFeedhData();
-		fetchBookmarkData();
 	}, [user]);
 
 	const MoveAlrimPage = () => {
@@ -130,163 +110,148 @@ const IndexPage = observer(() => {
 				</Tab>
 			</TabsWrap>
 			<div>
-				{selectedTabs === "feed" && <Feed data={feedData} />}
-				{selectedTabs === "bookmark" && <Feed data={likePost} />}
+				{selectedTabs === "feed" && <Feed clickedPage="feed" />}
+				{selectedTabs === "bookmark" && <Feed clickedPage="bookmark" />}
 			</div>
-			{/* <input
-        onChange={(value) => {
-          userStore.setName(value.target.value);
-        }}
-      /> */}
 		</Wrapper>
 	);
 });
 
 interface ITitleType {
-  count: number;
-  title: string;
-  onPageChange: any;
+	count: number;
+	title: string;
+	onPageChange: any;
 }
 
 const MyActivity = ({ count, title, onPageChange }: ITitleType) => {
-  return (
-    <Activity onClick={() => onPageChange()}>
-      <Count>{count}</Count>
-      <Title>{title}</Title>
-    </Activity>
-  );
+	return (
+		<Activity onClick={() => onPageChange()}>
+			<Count>{count}</Count>
+			<Title>{title}</Title>
+		</Activity>
+	);
 };
 
 export default withMain(IndexPage, "마이페이지");
 
 const Wrapper = styled.div`
-  width: 100%;
-  height: 100%;
+	width: 100%;
+	height: 100%;
 `;
 
 const Padding = styled.div`
-  padding: 0 20px;
+	padding: 0 20px;
 `;
 
 const MyInfo = styled.div`
-  padding: 25px 40px;
-
-  @media screen and (max-width: 600px) {
-    padding: 15px 20px;
-  }
+	padding: 25px 40px;
+	@media screen and (max-width: 600px) {
+		padding: 15px 20px;
+	}
 `;
 
 const Activity = styled.div`
-  text-align: center;
-  cursor: pointer;
+	text-align: center;
+	cursor: pointer;
 `;
 
 const Count = styled.div`
-  font-size: 22px;
-  font-weight: bold;
-  color: #000;
-
-  @media screen and (max-width: 600px) {
-    word-break: break-all;
-    font-size: 18px;
-  }
+	font-size: 22px;
+	font-weight: bold;
+	color: #000;
+	@media screen and (max-width: 600px) {
+		word-break: break-all;
+		font-size: 18px;
+	}
 `;
 
 const Title = styled.div`
-  font-size: 18px;
-  font-weight: 400;
-  color: #000;
-
-  @media screen and (max-width: 600px) {
-    font-size: 16px;
-  }
+	font-size: 18px;
+	font-weight: 400;
+	color: #000;
+	@media screen and (max-width: 600px) {
+		font-size: 16px;
+	}
 `;
 
 const ProfileWrap = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
 `;
 
 const ProfileImg = styled.img`
-  width: 90px;
-
-  border-radius: 100%;
-
-  @media screen and (max-width: 600px) {
-    width: 60px;
-  }
+	width: 90px;
+	border-radius: 100%;
+	@media screen and (max-width: 600px) {
+		width: 60px;
+	}
 `;
 
 const NickName = styled.div`
-  margin-top: 20px;
-  font-size: 24px;
-  font-weight: bold;
-
-  @media screen and (max-width: 600px) {
-    font-size: 16px;
-  }
+	margin-top: 20px;
+	font-size: 24px;
+	font-weight: bold;
+	@media screen and (max-width: 600px) {
+		font-size: 16px;
+	}
 `;
 
 const Intro = styled.div`
-  font-size: 20px;
-
-  @media screen and (max-width: 600px) {
-    word-break: break-all;
-    font-size: 16px;
-  }
+	font-size: 20px;
+	@media screen and (max-width: 600px) {
+		word-break: break-all;
+		font-size: 16px;
+	}
 `;
 
 const EditProfile = styled.button`
-  color: ${theme.PrimaryColor};
-  border: 1px solid ${theme.SecondaryColor};
-  border-radius: 3px;
-  width: 100%;
-  height: 50px;
-  display: block;
-  font-size: 20px;
-
-  @media screen and (max-width: 600px) {
-    font-size: 16px;
-    height: 40px;
-  }
+	color: ${theme.PrimaryColor};
+	border: 1px solid ${theme.SecondaryColor};
+	border-radius: 3px;
+	width: 100%;
+	height: 50px;
+	display: block;
+	font-size: 20px;
+	@media screen and (max-width: 600px) {
+		font-size: 16px;
+		height: 40px;
+	}
 `;
 
 const LogoutBtn = styled(EditProfile)`
-  color: ${theme.TextSubColor};
-  border: 1px solid ${theme.TextSubColor};
-  margin-top: 10px;
+	color: ${theme.TextSubColor};
+	border: 1px solid ${theme.TextSubColor};
+	margin-top: 10px;
 `;
 
 const TabsWrap = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
 `;
 
 const Tab = styled.div`
-  width: 50%;
-  padding: 20px;
-  font-size: 20px;
-  text-align: center;
-  line-height: 24px;
-  cursor: pointer;
-  transition: all 200ms ease;
-  border-bottom: ${(props: { selectedTabs: string; page: string }) =>
-    props.selectedTabs === props.page
-      ? `2px solid ${theme.PrimaryColor}`
-      : "2px solid #EBEBEB"};
-  color: ${(props: { selectedTabs: string; page: string }) =>
-    props.selectedTabs === props.page ? `${theme.PrimaryColor}` : "#000"};
-  font-weight: ${(props: { selectedTabs: string; page: string }) =>
-    props.selectedTabs === props.page ? 600 : 500};
-
-  &:hover {
-    color: ${theme.PrimaryColor};
-  }
-
-  @media screen and (max-width: 600px) {
-    padding: 14px;
-    font-size: 16px;
-  }
+	width: 50%;
+	padding: 20px;
+	font-size: 20px;
+	text-align: center;
+	line-height: 24px;
+	cursor: pointer;
+	transition: all 200ms ease;
+	border-bottom: ${(props: { selectedTabs: string; page: string }) =>
+		props.selectedTabs === props.page
+			? `2px solid ${theme.PrimaryColor}`
+			: "2px solid #EBEBEB"};
+	color: ${(props: { selectedTabs: string; page: string }) =>
+		props.selectedTabs === props.page ? `${theme.PrimaryColor}` : "#000"};
+	font-weight: ${(props: { selectedTabs: string; page: string }) =>
+		props.selectedTabs === props.page ? 600 : 500};
+	&:hover {
+		color: ${theme.PrimaryColor};
+	}
+	@media screen and (max-width: 600px) {
+		padding: 14px;
+		font-size: 16px;
+	}
 `;
